6c701e75454480c33e845f6ab8d2b7535de3f9dc
Tommaso <65722261+TommasoTate@users.noreply.github.com>
2024-04-05 20:47:55 +0200

    boostrapping again with rn-reusables

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,33 +1,79 @@
-import { Theme, ThemeProvider } from "@react-navigation/native";
-import { SplashScreen, Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import React, { useEffect } from "react";
-import { NAV_THEME } from "~/lib/constants";
-import { useColorScheme } from "~/lib/useColorScheme";
-import 'global.css'
+import '~/global.css';
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Theme, ThemeProvider } from '@react-navigation/native';
+import { SplashScreen, Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { Platform } from 'react-native';
+import { NAV_THEME } from '~/lib/constants';
+import { useColorScheme } from '~/lib/useColorScheme';
+import { PortalHost } from '~/components/primitives/portal';
+import { ThemeToggle } from '~/components/ThemeToggle';
+
+const LIGHT_THEME: Theme = {
+  dark: false,
+  colors: NAV_THEME.light,
+};
 const DARK_THEME: Theme = {
   dark: true,
   colors: NAV_THEME.dark,
 };
 
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
+
 // Prevent the splash screen from auto-hiding before getting the color scheme.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const { setColorScheme } = useColorScheme();
+  const { colorScheme, setColorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
 
-  useEffect(() => {
-    setColorScheme('dark')
-    SplashScreen.hideAsync();
+  React.useEffect(() => {
+    (async () => {
+      const theme = await AsyncStorage.getItem('theme');
+      if (Platform.OS === 'web') {
+        // Adds the background color to the html element to prevent white background on overscroll.
+        document.documentElement.classList.add('bg-background');
+      }
+      if (!theme) {
+        AsyncStorage.setItem('theme', colorScheme);
+        setIsColorSchemeLoaded(true);
+        return;
+      }
+      const colorTheme = theme === 'dark' ? 'dark' : 'light';
+      if (colorTheme !== colorScheme) {
+        setColorScheme(colorTheme);
+
+        setIsColorSchemeLoaded(true);
+        return;
+      }
+      setIsColorSchemeLoaded(true);
+    })().finally(() => {
+      SplashScreen.hideAsync();
+    });
   }, []);
 
+  if (!isColorSchemeLoaded) {
+    return null;
+  }
+
   return (
-    <ThemeProvider value={DARK_THEME}>
-      <StatusBar style={'dark'} />
+    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+      <StatusBar style={isDarkColorScheme ? 'light' : 'dark'} />
       <Stack>
-        <Stack.Screen name="(main)" options={{ statusBarHidden: true }} />
+        <Stack.Screen
+          name='index'
+          options={{
+            title: 'Starter Base',
+            headerRight: () => <ThemeToggle />,
+          }}
+        />
       </Stack>
+      <PortalHost />
     </ThemeProvider>
   );
 }
--- lib/useColorScheme.tsx
+++ lib/useColorScheme.tsx
@@ -1,12 +1,10 @@
-import { useColorScheme as useNativewindColorScheme } from "nativewind";
+import { useColorScheme as useNativewindColorScheme } from 'nativewind';
 
 export function useColorScheme() {
-  const { colorScheme, setColorScheme, toggleColorScheme } =
-    useNativewindColorScheme();
-  console.log("colorScheme", colorScheme);
+  const { colorScheme, setColorScheme, toggleColorScheme } = useNativewindColorScheme();
   return {
-    colorScheme: colorScheme ?? "dark",
-    isDarkColorScheme: colorScheme === "dark",
+    colorScheme: colorScheme ?? 'dark',
+    isDarkColorScheme: colorScheme === 'dark',
     setColorScheme,
     toggleColorScheme,
   };
