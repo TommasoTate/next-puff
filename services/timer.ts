3f235cce34691d20881a5b95ccd6a9b9bdd6740c
Tommaso <65722261+TommasoTate@users.noreply.github.com>
2024-04-07 22:49:55 +0200

    fixed timer service

--- services/timer.ts
+++ services/timer.ts
@@ -1,10 +1,11 @@
 import dayjs from 'dayjs'
 
-const DAILY_PUFFS = 12
-const FIRST_PUFF_DATE = dayjs().startOf('day').add(8, 'hour').toDate()
-const LAST_PUFF_DATE = dayjs().startOf('day').add(23, 'hour').toDate()
 
-// puff interval in milliseconds between each puff starting from FIRST_PUFF_DATE to LAST_PUFF_DATE
-const PUFF_INTERVAL = dayjs(LAST_PUFF_DATE).diff(FIRST_PUFF_DATE) / DAILY_PUFFS
-
-export const getPuffInterval = () =>  PUFF_INTERVAL
+export const getPuffInterval = (firstPuffHour: number, LastPuffHour: number, dailyPuffs: number) =>  {
+    if (firstPuffHour > LastPuffHour) {
+        throw new Error('First puff hour must be less than last puff hour')
+    }
+    const firstPuff = dayjs().hour(firstPuffHour).minute(0).second(0)
+    const lastPuff = dayjs().hour(LastPuffHour).minute(0).second(0)
+    return lastPuff.diff(firstPuff, 'minute') / dailyPuffs
+}
